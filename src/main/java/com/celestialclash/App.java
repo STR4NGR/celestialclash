0b5407ce6d6cecc43be1ef90180846e387a7c07d
STR4NGR <kerrickinc@gmail.com>
2024-04-30 15:20:56 +0700

    preparing plugin

---
+++ src/main/java/com/celestialclash/App.java
@@ -0,0 +1,15 @@
+package com.celestialclash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
